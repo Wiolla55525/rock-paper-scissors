ec182de209c9981821974d2fbeb923dd4bbda4e0
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-08-30 21:41:50 +0300

    feat: created game function

--- script/script.js
+++ script/script.js
@@ -1,8 +1,31 @@
-const options = ['rock', 'paper', 'scissors'];
 function getComputerChoice() {
+    const options = ['rock', 'paper', 'scissors'];
     return options[Math.floor(Math.random() * options.length)];
 }
 
+
+function game(playerSelection, computerSelection){
+    playRound();
+    if (playerSelection === computerSelection) {
+        return "Tie";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You loose :( Paper beats Rock!"
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats Paper!"
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You loose :( Scissors beats Paper!"
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You win! Paper beats Rock!"
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You loose :( Scissors beats Rock!"
+    } else {
+        return "You win! Scissors beats Paper!"
+    }
+}
+game()
+function playRound() {
+
+}
 const playerSelection = 'rock';
 const computerSelection = getComputerChoice();
 
