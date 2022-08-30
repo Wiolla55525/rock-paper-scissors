abbca1c74b915eba5fba96d179aefea88dafebfd
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-08-30 20:59:46 +0300

    fix: function of random choice

--- script/script.js
+++ script/script.js
@@ -1,11 +1,12 @@
-const options = ['rock', 'paper', 'scissors']
-function getComputerChoice(playerSelection, computerSelection) {
-    if (playerSelection == 'rock'.length) {
-        return 
-    }
+const options = ['rock', 'paper', 'scissors'];
+function getComputerChoice() {
+    return options[Math.floor(Math.random() * options.length)];
 }
 
 const playerSelection = 'rock';
 const computerSelection = getComputerChoice();
+
+console.log(`Player selection: ${playerSelection}`);
+console.log(`Computer selection: ${computerSelection}`);
 
 console.log(getComputerChoice(playerSelection, computerSelection));
