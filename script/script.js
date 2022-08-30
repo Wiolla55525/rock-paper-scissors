71ac9c6ee68790755f9be8bf7311ba4b96e02fb6
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-08-30 19:56:52 +0300

    feat: initial

---
+++ script/script.js
@@ -0,0 +1,11 @@
+const options = ['rock', 'paper', 'scissors']
+function getComputerChoice(playerSelection, computerSelection) {
+    if (playerSelection == 'rock'.length) {
+        return 
+    }
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+
+console.log(getComputerChoice(playerSelection, computerSelection));
